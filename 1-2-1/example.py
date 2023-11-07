395c0223ec3dfcc559ed009b7761b6285dad4a5e
mrkiedes <akiedes@vtsd.com>
2023-11-07 09:55:29 -0500

    Triangle Update

---
+++ 1-2-1/example.py
@@ -0,0 +1,29 @@
+import turtle as trtl
+test = trtl.Turtle()
+
+def drawTriangle():
+    test.pendown()
+    test.fd(100)
+    test.right(120)
+    test.fd(100)
+    test.right(120)
+    test.fd(100)
+
+def drawTriangle(long, x, y):
+    test.penup()
+    test.goto(x, y)
+    test.pendown()
+    test.fd(long)
+    test.right(120)
+    test.fd(long)
+    test.right(120)
+    test.fd(long)
+
+x = 100
+y = 100
+for tri in range(100):
+    drawTriangle()
+
+
+wn = trtl.Screen()
+wn.mainloop()
