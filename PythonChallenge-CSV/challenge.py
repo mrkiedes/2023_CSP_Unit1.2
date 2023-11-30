7f4948d97bd6cb6d3e9104b9e594752a528b04a1
mrkiedes <akiedes@vtsd.com>
2023-11-30 08:38:11 -0500

    CSV Programming Challenege Update

---
+++ PythonChallenge-CSV/challenge.py
@@ -0,0 +1,33 @@
+'''
+This file contains a string with a set of CSV (comma separated values) data.
+
+Part 1: Using a loop, break apart the CSV and store each value in a list
+    * A loop similar to last unit may help break apart the string
+
+Part 2: Display the contents of the list showing the index number and the value on screen.
+
+Example: If my string is "John,Jill,Dave" then my list should store John in index 0, Jill in index 1, and Dave in index 2
+Example Output:
+0 - John
+1 - Jill
+2 - Dave
+
+No values (other than the string I provide) may be hardcoded.
+In other words, if I change the string at the top to something different your program should still work.
+
+Note: There are no turtles in this project.  All output should be to the console using print statements.
+
+Good luck - you have the period!
+
+'''
+
+# Initial set of CSV values
+values ="KingsIndian,QueensGambit,TheColle,TheFrench,TheFriedLiver,TheFourKnights"
+
+#Do Part 1 of the challenge below here
+
+
+
+
+
+#Do part 2 of the challenge below here
