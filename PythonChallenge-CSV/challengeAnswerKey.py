6fc5270b488a363137489d3a3655979ccb8980c4
mrkiedes <akiedes@vtsd.com>
2023-12-04 09:18:16 -0500

    Challenge Solution Update

--- PythonChallenge-CSV/challengeAnswerKey.py
+++ PythonChallenge-CSV/challengeAnswerKey.py
@@ -22,11 +22,23 @@ Good luck - you have the period!
 '''
 
 # Initial set of CSV values
-csv ="KingsIndian,QueensGambit,TheColle,TheFrench,TheFriedLiver,TheFourKnights"
+csv ="FourKnights,KingsIndian,QueensGambit,TheColle,TheFrench,TheFriedLiver,TheFourKnights"
+
+
+value =""
 index = 0
 myValues = []
-val = ""
-
 #Do Part 1 of the challenge below here
+for index in range(len(csv)):
+    if index == len(csv):
+        myValues.append(value)
+    else:
+        if(csv[index] != ","):
+            value += csv[index]
+        else:
+            myValues.append(value)
+            value = ""
 
 #Do part 2 of the challenge below here
+for index in range(len(myValues)):
+    print(str(index) + " - " + myValues[index])
