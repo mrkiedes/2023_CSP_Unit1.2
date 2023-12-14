6844201001eef68c18daf2ef394c792daa4eb73e
mrkiedes <akiedes@vtsd.com>
2023-12-14 07:51:34 -0500

    1.2.3 Through Step 15 Base Code

--- 1-2-3/main.py
+++ 1-2-3/main.py
@@ -14,26 +14,18 @@ wn.addshape(apple_image)  # Make the screen aware of the new file
 wn.addshape(pear_image)  # Make the screen aware of the new file
 apple_letter_x_offset = 25
 apple_letter_y_offset = 50
-screen_width = 400
-screen_height = 400
-
 letter_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
                'v', 'w', 'x', 'y', 'z']
 
 apple = trtl.Turtle()
 apple.penup()
 wn.tracer(False)
-
-
 # -----functions-----
-
 def draw_apple(active_apple, letter):
     active_apple.showturtle()
     active_apple.shape(apple_image)
     drawLetter(active_apple, letter)
     wn.update()
-
-
 
 def drawLetter(active_apple, letter):
     drawer.penup()
