3423debaed9fc17f291b81c96330179fd56467c6
mrkiedes <akiedes@vtsd.com>
2023-12-14 07:50:47 -0500

    1.2.3 Through Step 15 Base Code

--- 1-2-3/main.py
+++ 1-2-3/main.py
@@ -14,8 +14,11 @@ wn.addshape(apple_image)  # Make the screen aware of the new file
 wn.addshape(pear_image)  # Make the screen aware of the new file
 apple_letter_x_offset = 25
 apple_letter_y_offset = 50
+screen_width = 400
+screen_height = 400
 
-letterList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+letter_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z']
 
 apple = trtl.Turtle()
 apple.penup()
@@ -23,73 +26,40 @@ wn.tracer(False)
 
 
 # -----functions-----
-#TODO Create a function that takes a turtle as its parameter and gives that turtle (apple)
-# a new location on the tree, only if the list of letters is not empty. Associate the
-# turtle with a new letter selected at random from the list of letters
-def draw_apple(active_apple):
-    active_apple.shape(apple_image)
-    active_apple.penup()
-    if letterList:
-        newLetter = rand.choice(letterList)
-        letterList.remove(newLetter)
-        newX = rand.randint(-270, 270)
-        active_apple.goto(newX, 160)
-        drawLetter(active_apple, newLetter)
-        wn.update()
 
-#TODO Create a function that takes a turtle (apple) and its corresponding letter from the letter
-# list and draws that letter on that turtle (apple)
+def draw_apple(active_apple, letter):
+    active_apple.showturtle()
+    active_apple.shape(apple_image)
+    drawLetter(active_apple, letter)
+    wn.update()
+
+
+
 def drawLetter(active_apple, letter):
     drawer.penup()
     drawer.goto(active_apple.xcor() - apple_letter_x_offset, active_apple.ycor() - apple_letter_y_offset)
     drawer.color("white")
+    drawer.clear()
     drawer.write(letter, font=("Arial", 55, "bold"))
 
-#TODO Create a function that takes a turtle (apple) and its corresponding ltter from the letter
-# list and set that turtle to be shaped by the image file, call the letter drawing function,
-# and update the Screen
-
-
-
-#TODO Iterate over the numbers from 0 to the number of apples, creating that many turtles
-# calling your function that resets the apples by giving them a new random location
-# add the new apples to a list of apples to be used in the rest of the program.
-# The loop below executes the correct number of times by using the range() function
-# to create a list of numbers to iterate over.
-#for i in range(0, number_of_apples):
-  #Your code here
-
-#TODO Create a function that takes a letter as its parameter, uses that letter to retrieve the
-# corresponding turtle (apple) and causes both to drop from the tree simultaneously. Once the
-# apple and letter have dropped, call the apple reseting function.
-
-#TODO define a function per letter that you will use in your program. Each function should check
-# to see if the given letter is in the list of letters; if it is, it should drop the corresponding
-# apple.
-
-#TODO use the onkeypress method of wn to correlate the functions you defined above with each
-# of the letters that the user might type.
-# onkeypress requires that you name one function that must take
-# no arguments to be called when the specified key is pressed.
-
-
-
+def reset_apple(active_apple):
+    if letter_list:
+        letter = rand.choice(letter_list)
+        letter_list.remove(letter)
+        active_apple.goto(rand.randint(-270, 270), 160)
+        print(letter_list)
+        draw_apple(active_apple, letter)
 
 def drop_apple():
     wn.tracer(True)
     apple.goto(apple.xcor(), apple.ycor() - 400)
     apple.hideturtle()
     wn.tracer(False)
-
-
-
-
-
-
+    reset_apple(apple)
 
 
 # -----function calls-----
-draw_apple(apple)
+draw_apple(apple, "A")
 wn.onkeypress(drop_apple, "a")
 
 wn.listen()
